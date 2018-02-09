87cd556042fd731364202c8fa0d2a5baa01c06af
JordaniJP <35529785+JordaniJP@users.noreply.github.com>
2018-02-08 22:51:46 -0500

    Completed First Version of Prisoners Dilemma
    This is not the final version. I will patch it up later.

--- team3.py
+++ team3.py
@@ -25,8 +25,15 @@ def move(my_history, their_history, my_score, their_score):
     
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
-    
-    return 'c'
+    if len(my_history)==0:
+        return 'c'
+        if len(their_history)>150:
+            return 'b'
+        elif their_history[-1]=='b':
+            return 'b'
+        elif their_history[-1]=='c':
+            return'c'
+          
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
